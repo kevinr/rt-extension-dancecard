dbab07242279c489df920d7ecb97e1240e2d57cb
Kevin Riggle <kevinr@free-dissociation.com>
2013-09-18 06:23:19 +0000

    module skeleton

---
+++ lib/RT/Extension/Dancecard.pm
@@ -0,0 +1,32 @@
+package RT::Extension::Dancecard;
+
+our $VERSION = '0.01';
+
+use warnings;
+use strict;
+
+
+=head1 NAME
+
+RT::Extension::Dancecard
+
+=head1 SYNOPSIS
+
+Various and sundry code required by the RT backend for Dancecard
+
+=cut
+
+1;
+
+=head1 AUTHOR
+
+Kevin Riggle  C<< <kevinr@free-dissociation.com> >>
+
+
+=head1 LICENCE AND COPYRIGHT
+
+Copyright (c) 2013 Kevin Riggle.  All rights reserved.
+
+=cut
+
+1;
---
+++ t/00.load.t
@@ -0,0 +1,7 @@
+use Test::More tests => 1;
+
+BEGIN {
+use_ok( 'RT::Extension::Dancecard' );
+}
+
+diag( "Testing RT::Extension::Dancecard $RT::Extension::Dancecard::VERSION" );
